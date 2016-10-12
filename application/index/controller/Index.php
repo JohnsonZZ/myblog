00f89fa9b029bae3c5ee2432dc6379c1f04c82be
JohnsonZZ <465629989@qq.com>
2016-10-12 14:01:07 +0800

    oauth

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,13 +7,13 @@ class index extends Com
 {
     public function index()
     {
-		
 		$articles = new Articles();
 		$article = $articles
 					->alias('a')
 					->field('a.id,photo,brief,title,a.time,pcount,like,count(c.id) as review')
 					->join('comments c','a.id=c.aid','LEFT')
-					->group('a.id') 
+					->group('a.id')
+					->order('rand()')					
 					->select();
 		$this->assign('articles',$article);
 		return $this->fetch();
