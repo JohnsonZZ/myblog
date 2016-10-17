ac6f560edbf83f21c3091ca6d35b22b6c01ce52b
JohnsonZZ <465629989@qq.com>
2016-10-17 15:30:45 +0800

    404

--- application/config.php
+++ application/config.php
@@ -234,7 +234,15 @@ return [
         // 是否使用 setcookie
         'setcookie' => true,
     ],
-
+	//错误重定向
+	'http_exception_template'    =>  [
+		// 定义404错误的重定向页面地址
+		404 =>  APP_PATH.'404.html',
+		// 还可以定义其它的HTTP status
+		401 =>  APP_PATH.'401.html',
+		
+		500 =>  APP_PATH.'500.html',
+	],
     //分页配置
     'paginate'               => [
         'type'      => 'bootstrap',
